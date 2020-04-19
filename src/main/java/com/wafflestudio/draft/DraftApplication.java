8f2569507c1d99ba5b518228ad39c9c85a1ac990
Hank Choi <zlzlqlzl1@naver.com>
2020-04-19 21:37:33 +0900

    spring boot init

---
+++ src/main/java/com/wafflestudio/draft/DraftApplication.java
@@ -0,0 +1,13 @@
+package com.wafflestudio.draft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DraftApplication.class, args);
+	}
+
+}
