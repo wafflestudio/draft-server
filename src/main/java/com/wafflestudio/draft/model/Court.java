c84dee0eecd49efcf1d6d510c341e11c65ff162c
Hank Choi <zlzlqlzl1@naver.com>
2020-04-26 21:47:11 +0900

    update gradle setting files

--- src/main/java/com/wafflestudio/draft/model/Court.java
+++ src/main/java/com/wafflestudio/draft/model/Court.java
@@ -19,7 +19,7 @@ public class Court {
     private Long id;
 
     @ManyToOne
-    @JoinColumn(name = "region_id")
+    @JoinColumn(name = "region_id",referencedColumnName = "region_id")
     private Region region;
 
     @Column
