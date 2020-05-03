f12c7a664229d6160baafc71019303d054ad3e7c
Davin <bdv111@snu.ac.kr>
2020-05-04 06:27:47 +0900

    revert unnecessary changes

--- src/main/java/com/wafflestudio/draft/config/SecurityConfig.java
+++ src/main/java/com/wafflestudio/draft/config/SecurityConfig.java
@@ -14,8 +14,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
- @Configuration
- @EnableWebSecurity
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
