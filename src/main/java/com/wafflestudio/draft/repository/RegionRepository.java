b172bc2f8ab6834eaba0de3bcda1f3fa97870a92
Hank Choi <48513130+zlzlqlzl@users.noreply.github.com>
2020-05-16 11:45:27 +0900

    Feature/#13 (#16)
    * Add files related to Preference
    
    * PreferenceRepository, PreferenceService 추가
    * UserService.currentUser
    * UserController 추가
    
    * Update files for user's preference
    
    * Change idea setting
    
    * rm idea from git
    
    * modify gitignore
    
    Co-authored-by: Davin <bdv111@snu.ac.kr>

--- src/main/java/com/wafflestudio/draft/repository/RegionRepository.java
+++ src/main/java/com/wafflestudio/draft/repository/RegionRepository.java
@@ -4,4 +4,5 @@ import com.wafflestudio.draft.model.Region;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RegionRepository extends JpaRepository<Region, Long> {
+    Region findByName(String name);
 }
