8a0cb1568c708e538f9a6dd71ca75477362b47c0
Davin <bdv111@snu.ac.kr>
2020-08-02 01:06:27 +0900

    first controller test

--- src/test/java/com/wafflestudio/draft/service/RoomServiceTest.java
+++ src/test/java/com/wafflestudio/draft/service/RoomServiceTest.java
@@ -3,34 +3,40 @@ package com.wafflestudio.draft.service;
 import com.wafflestudio.draft.model.Court;
 import com.wafflestudio.draft.model.Room;
 import com.wafflestudio.draft.model.User;
+import com.wafflestudio.draft.repository.CourtRepository;
 import com.wafflestudio.draft.repository.RoomRepository;
+import com.wafflestudio.draft.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@ActiveProfiles("test")
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @Transactional
 class RoomServiceTest {
 
     @Autowired RoomService roomService;
-    @Autowired UserService userService;
-    @Autowired CourtService courtService;
     @Autowired RoomRepository roomRepository;
+    @Autowired UserRepository userRepository;
+    @Autowired CourtRepository courtRepository;
 
     @Test
     void create() throws Exception {
         // given
         Room room = new Room();
-        User user = userService.findUserByEmail("authuser@test.com").get();
+        User user = new User("TEST_USER", "user@test.com");
+        userRepository.save(user);
         room.setOwner(user);
 
-        Court court = courtService.getCourtById(1L).get();
+        Court court = new Court();
+        courtRepository.save(court);
         room.setCourt(court);
 
         // when
