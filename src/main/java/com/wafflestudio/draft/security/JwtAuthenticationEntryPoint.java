a484ffddba5416f393d73f532508bc44fae1f809
sanggggg <ksme6776@gmail.com>
2020-08-01 13:36:37 +0900

    handle 401 on full authentication needed

--- src/main/java/com/wafflestudio/draft/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/wafflestudio/draft/security/JwtAuthenticationEntryPoint.java
@@ -12,6 +12,6 @@ import java.io.IOException;
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
-        response.sendError(response.getStatus(), authException.getMessage());
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
     }
 }
