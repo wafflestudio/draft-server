c492c2032b37f5180f21a1c4d641b22e9a8d353e
Davin Byeon <bdv111@snu.ac.kr>
2020-08-03 00:11:24 +0900

    first controller test (#62)
    Co-authored-by: Hank Choi <zlzlqlzl1@naver.com>

--- src/main/java/com/wafflestudio/draft/DataLoader.java
+++ src/main/java/com/wafflestudio/draft/DataLoader.java
@@ -13,11 +13,13 @@ import com.wafflestudio.draft.service.RoomService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 
+@Profile("!test")
 @Component
 @RequiredArgsConstructor
 public class DataLoader implements ApplicationRunner {
--- src/test/java/com/wafflestudio/draft/service/UserAuthTest.java
+++ src/test/java/com/wafflestudio/draft/service/UserAuthTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MockMvcBuilder;
@@ -15,6 +16,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+@ActiveProfiles("test")
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class UserAuthTest {
