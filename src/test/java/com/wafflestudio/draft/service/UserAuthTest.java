921a32a9cd3ac8d5bd791f371bdeb602b6a7d878
Hank Choi <zlzlqlzl1@naver.com>
2020-10-14 01:23:22 +0900

    Apply PR#62

--- src/test/java/com/wafflestudio/draft/service/UserAuthTest.java
+++ src/test/java/com/wafflestudio/draft/service/UserAuthTest.java
@@ -1,9 +1,22 @@
 package com.wafflestudio.draft.service;
 
+import com.wafflestudio.draft.api.UserApiController;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+@ActiveProfiles("test")
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class UserAuthTest {
