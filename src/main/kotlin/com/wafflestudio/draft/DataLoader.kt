599f4ae57f59f52dd24b948b50365b53e1beea49
Hank Choi <zlzlqlzl1@naver.com>
2020-11-24 22:05:56 +0900

    Resolve failed test

--- src/main/kotlin/com/wafflestudio/draft/DataLoader.kt
+++ src/main/kotlin/com/wafflestudio/draft/DataLoader.kt
@@ -30,7 +30,7 @@ class DataLoader(val userRepository: UserRepository, val regionRepository: Regio
         val gsf = GeometricShapeFactory()
         val testPolygon = gf.createMultiPolygon(arrayOf<Polygon>(gsf.createCircle(), gsf.createRectangle()))
         testPolygon.srid = 5179
-        val testRegion = Region(null, 99999999, null, null, "테스트", testPolygon, null)
+        val testRegion = Region(null, 99999999, null, null, "테스트", testPolygon, "TEST_REGION")
         regionRepository.save(testRegion)
 
         val testCourt = Court(region = testRegion, name = "TEST_COURT", capacity = 10, location = point)
