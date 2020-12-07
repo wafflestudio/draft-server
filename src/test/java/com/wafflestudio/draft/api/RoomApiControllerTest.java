70fbc9118388a6b3a89a37d14590872b3cb5b731
Hank Choi <zlzlqlzl1@naver.com>
2020-12-08 01:54:41 +0900

    Resolve test errors

--- src/test/java/com/wafflestudio/draft/api/RoomApiControllerTest.java
+++ src/test/java/com/wafflestudio/draft/api/RoomApiControllerTest.java
@@ -49,14 +49,15 @@ public class RoomApiControllerTest {
         this.mockMvc.perform(get("/api/v1/room/")
                 .param("name", "TEST_ROOM_3").contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$", hasSize(1)))
-                .andExpect(jsonPath("$[0].id", is(3)))
-                .andExpect(jsonPath("$[0].name", is("TEST_ROOM_3")));
+                .andExpect(jsonPath("$['results']", hasSize(1)))
+                .andExpect(jsonPath("$['results'][0].id", is(3)))
+                .andExpect(jsonPath("$['results'][0].name", is("TEST_ROOM_3")));
 
         this.mockMvc.perform(get("/api/v1/room/")
                 .param("regionId", "1").contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$", hasSize(5)));
+                .andExpect(jsonPath("$['results']", hasSize(5)))
+                .andExpect(jsonPath("$['count']", is(5)));
 
         MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
         params.add("name", "TEST_ROOM_4");
@@ -64,33 +65,33 @@ public class RoomApiControllerTest {
         this.mockMvc.perform(get("/api/v1/room/")
                 .params(params).contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$", hasSize(1)))
-                .andExpect(jsonPath("$[0].name", is("TEST_ROOM_4")));
+                .andExpect(jsonPath("$['results']", hasSize(1)))
+                .andExpect(jsonPath("$['results'][0].name", is("TEST_ROOM_4")));
 
         String oneMinuteBefore = LocalDateTime.now().minusMinutes(1).format(
                                     DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss"));
         this.mockMvc.perform(get("/api/v1/room/")
                 .param("startTime", oneMinuteBefore).contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$", hasSize(5)));
+                .andExpect(jsonPath("$['results']", hasSize(5)));
 
         String oneMinuteAfter = LocalDateTime.now().plusMinutes(1).format(
                 DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss"));
         this.mockMvc.perform(get("/api/v1/room/")
                 .param("startTime", oneMinuteAfter).contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$", hasSize(0)));
+                .andExpect(jsonPath("$['results']", hasSize(0)));
 
         String oneDayAfter = LocalDateTime.now().plusDays(1).format(
                 DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss"));
         this.mockMvc.perform(get("/api/v1/room/")
                 .param("endTime", oneDayAfter).contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$", hasSize(5)));
+                .andExpect(jsonPath("$['results']", hasSize(5)));
 
         this.mockMvc.perform(get("/api/v1/room/")
                 .param("endTime", oneMinuteAfter).contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$", hasSize(0)));
+                .andExpect(jsonPath("$['results']", hasSize(0)));
     }
 }
