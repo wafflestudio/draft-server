39932ca25f0aa4a4463c9ff598c1bea90709c6a2
Davin Byeon <bdv111@snu.ac.kr>
2020-12-22 02:31:43 +0900

    하나의 User가 같은 시간에 진행되는 여러 Room에 참여하지 못하도록 하는 로직 구현 (#84)
    * use JpaRepository for RoomRepository & rest api errorhandling
    
    * remove unnecessary lines
    
    * no concurrently participating multiple rooms

--- src/main/kotlin/com/wafflestudio/draft/service/RoomService.kt
+++ src/main/kotlin/com/wafflestudio/draft/service/RoomService.kt
@@ -3,6 +3,7 @@ package com.wafflestudio.draft.service
 import com.wafflestudio.draft.model.Room
 import com.wafflestudio.draft.model.User
 import com.wafflestudio.draft.error.RoomNotFoundException
+import com.wafflestudio.draft.error.WrongRoomTimeException
 import com.wafflestudio.draft.repository.RoomRepository
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -12,9 +13,15 @@ import java.time.LocalDateTime
 @Transactional(readOnly = true)
 class RoomService(private val roomRepository: RoomRepository) {
     @Transactional
-    fun save(room: Room): Long? {
+    fun save(room: Room): Long {
+        val startTimeExists = room.startTime != null
+        val endTimeExists = room.endTime != null
+        if ((startTimeExists xor endTimeExists)
+                || (startTimeExists && endTimeExists && room.startTime!! >= room.endTime)) {
+            throw WrongRoomTimeException()
+        }
         roomRepository.save(room)
-        return room.id
+        return room.id!!
     }
 
     fun findRooms(name: String?, regionId: Long?, startTime: LocalDateTime?, endTime: LocalDateTime?): List<Room>? {
@@ -32,5 +39,15 @@ class RoomService(private val roomRepository: RoomRepository) {
 
     fun findRoomsByUser(user: User): List<Room>? {
         return roomRepository.findByParticipatingUser(user)
+    }
+
+    fun existsCurrentlyParticipating(user: User, startTime: LocalDateTime, endTime: LocalDateTime): Boolean {
+        return roomRepository.existsByParticipatingUserAndStartTimeAndEndTime(user, startTime, endTime)
+    }
+
+    fun existsCurrentlyParticipatingExcludingRoom(
+            user: User, startTime: LocalDateTime, endTime: LocalDateTime, thisRoom: Room
+    ): Boolean {
+        return roomRepository.existsByParticipatingUserAndStartTimeAndEndTimeAndRoomNot(user, startTime, endTime, thisRoom)
     }
 }
