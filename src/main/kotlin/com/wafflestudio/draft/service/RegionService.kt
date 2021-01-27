dd51c708cc372dd1129ec538a7f32e085968d1bd
Davin Byeon <bdv111@snu.ac.kr>
2021-01-27 23:24:35 +0900

    pagination for region API (#91)

--- src/main/kotlin/com/wafflestudio/draft/service/RegionService.kt
+++ src/main/kotlin/com/wafflestudio/draft/service/RegionService.kt
@@ -3,6 +3,8 @@ package com.wafflestudio.draft.service
 import com.wafflestudio.draft.dto.RegionDTO
 import com.wafflestudio.draft.model.Region
 import com.wafflestudio.draft.repository.RegionRepository
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.util.*
@@ -19,8 +21,8 @@ class RegionService(private val regionRepository: RegionRepository) {
         return regionRepository.findByNameContainingWithoutGeometryData(name)
     }
 
-    fun findRegionsByDepth3(depth3: String?): List<RegionDTO.Summary> {
-        return regionRepository.findByDepth3ContainingWithoutGeometryData(depth3)
+    fun findRegionsByDepth3(depth3: String?, pageable: Pageable): Page<RegionDTO.Summary> {
+        return regionRepository.findByDepth3ContainingWithoutGeometryData(depth3, pageable)
     }
 
     fun getRegions(): List<RegionDTO.SummaryWithRooms> {
