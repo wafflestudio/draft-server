f86132dee2853717d8a6bc78caac4176f835b993
Davin <bdv111@snu.ac.kr>
2021-01-27 22:58:53 +0900

    pagination for region API

--- src/main/kotlin/com/wafflestudio/draft/api/RegionApiController.kt
+++ src/main/kotlin/com/wafflestudio/draft/api/RegionApiController.kt
@@ -2,12 +2,13 @@ package com.wafflestudio.draft.api
 
 import com.wafflestudio.draft.dto.RegionDTO
 import com.wafflestudio.draft.dto.response.ListResponse
+import com.wafflestudio.draft.dto.response.PageResponse
 import com.wafflestudio.draft.service.RegionService
-import com.wafflestudio.draft.service.RoomService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.data.domain.PageRequest
 import javax.validation.Valid
 
 @RestController
@@ -16,10 +17,13 @@ class RegionApiController(
         private val regionService: RegionService
 ) {
     @GetMapping("/")
-    fun getRegionsV1(@Valid @ModelAttribute request: RegionDTO.Request): ListResponse<RegionDTO.Response> {
+    fun getRegionsV1(@Valid @ModelAttribute request: RegionDTO.Request): PageResponse<RegionDTO.Summary> {
         val depth3 = request.depth3.orEmpty()
-        val regions = regionService.findRegionsByDepth3(depth3)
-        return ListResponse(regions.map { it.toResponse() })
+        val page = request.page
+        val pageable = PageRequest.of(page, 20)
+
+        val regions = regionService.findRegionsByDepth3(depth3, pageable)
+        return PageResponse(regions)
     }
 
     @GetMapping("/room/")
